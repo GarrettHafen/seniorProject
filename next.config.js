f3b9cd7b80e6873bce0c0a398541aa1942418a80
garretthafen <garrett.hafen12@gmail.com>
2019-07-01 21:14:42 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,18 @@
 const contentful = require("./helpers/contentful");
 
 module.exports = {
-    async exportPathMap() {
-      var pages = {
-        '/': { page: '/' },
-        '/login': { page: '/login'},
-        '/calendar': { page: '/calendar'},
-        '/signup': { page: '/signup'}
-      }
+  async exportPathMap() {
+    var pages = {
+      '/': { page: '/' },
+      '/login': { page: '/login' },
+      '/signup': { page: '/signup' }
+    }
     const listView = await contentful.query({ content_type: "listView" });
     listView.items.map(locale => {
       return Object.assign(pages, {
         [`/${locale.fields.pageUrl}`]: { page: "/listView" }
       });
     });
-      return pages
-    }
-  };
+    return pages
+  }
+};
