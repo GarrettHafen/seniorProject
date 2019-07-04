ffca61e10c2fc9aa997d8b46a5397cfb19b1df2d
Ian Clawson <ian@radiant.org>
2019-07-03 22:25:21 -0600

    thing

--- pages/index.js
+++ pages/index.js
@@ -1,158 +1,131 @@
-import React from 'react';
-import Nav from '../components/nav';
-import BannerImage from'../components/bannerImage';
-import Break from '../components/break';
-import Head from 'next/head';
-import Tile from '../components/tiles';
-import Instructions from '../components/instructions';
-import Third from '../components/third'
-
+import React from "react";
+import Nav from "../components/nav";
+import BannerImage from "../components/bannerImage";
+import Break from "../components/break";
+import Head from "next/head";
+import Tile from "../components/tiles";
+import Instructions from "../components/instructions";
+import Third from "../components/third";
 
 const logInSection = {
-    width: '65%',
-    height: '300px',
-    display: 'block',
-    margin: 'auto',
-    textAlign: 'center'
-}
+  width: "65%",
+  height: "300px",
+  display: "block",
+  margin: "auto",
+  textAlign: "center"
+};
 
 const middle = {
-    width: '30%',
-    height: '650px',
-    float: 'left',
-    margin: '25px',
-    display: 'flex',
-    margin: '25px',
-    flexDirection: 'column',
-    justifyContent: 'space-between'
-}
+  width: "30%",
+  height: "650px",
+  float: "left",
+  margin: "25px",
+  display: "flex",
+  margin: "25px",
+  flexDirection: "column",
+  justifyContent: "space-between"
+};
 
 const left = {
-    height: '650px',
-    width: '30%',
-    float: 'left',
-    margin: '25px',
-    background: 'linear-gradient( rgba(95,52,33, 0.85), rgba(95,52,33, 0.85) ),url("../static/uprightRoad.png") no-repeat',
-    borderRadius: '35px',
-    color: '#E1E1DA',
-    textAlign: 'center',
-    fontSize: '1.2em'
-}
+  height: "650px",
+  width: "30%",
+  float: "left",
+  margin: "25px",
+  background:
+    'linear-gradient( rgba(95,52,33, 0.85), rgba(95,52,33, 0.85) ),url("../static/uprightRoad.png") no-repeat',
+  borderRadius: "35px",
+  color: "#E1E1DA",
+  textAlign: "center",
+  fontSize: "1.2em"
+};
 
 const right = {
-    width: '30%',
-    height: '650px',
-    float: 'left',
-    margin: '25px',
-    display: 'flex',
-    margin: '25px',
-    flexDirection: 'column',
-    justifyContent: 'space-between'
-}
+  width: "30%",
+  height: "650px",
+  float: "left",
+  margin: "25px",
+  display: "flex",
+  margin: "25px",
+  flexDirection: "column",
+  justifyContent: "space-between"
+};
 
 const smallSection = {
-    height: '200px',
-    border: '1px solid blue',
-    margin: '5px'
-}
+  height: "200px",
+  border: "1px solid blue",
+  margin: "5px"
+};
 
 const Home = () => {
-    return(
-        <div>
-            <Head>
-                <link rel="stylesheet" href="/static/base.css"></link>
-            </Head>
-            <Nav></Nav>
-            <BannerImage
-                src='../static/bannerImageStorm.png'
-                alt='Stormy Image'
-                width='1704px'
-            />
-            <Break
-                src='hr-train.png'
-            />
-            <BannerImage
-                src='../static/carouselMist.png'
-                alt='Misty Image'
-                width='1278px'
-            />
-            <Break
-                src='hr-tumbleweed.png'
-            />
-            <div style={logInSection} className="logInSection">
-                <Third
-                    header="fire watches"
-                    name="Fire Warnings"
-                    src="static/hr-fish.png"
-                    paragarph="Fire Saftey is very important at the ranch, click here to check the current fire information."
-                    url="https://www.nevadafireinfo.org/"
-                    color="rgba(85, 135, 146, 0.5), rgba(107, 133, 62, 0.5)"
-                    target="_blank"
-                ></Third>
-                <Third
-                    header="login"
-                    name="Login"
-                    src="static/hr-blank.png"
-                    paragarph="---"
-                    url="/login"
-                    color="rgba(109, 94, 65, 0.5), rgba(85, 135, 146, 0.5)"
-                    target=""
-                ></Third>
-                <Third
-                    header="weather"
-                    name="Check The Weather"
-                    src="static/hr-fish.png"
-                    paragarph="Heading out soon? Click here to check the weather in the area."
-                    url="https://www.nevadafireinfo.org/"
-                    color="rgba(85, 135, 146, 0.5),rgba(107, 133, 62, 0.5)"
-                    target="_blank"
-                ></Third>
-            </div>
-            <Break
-                src='hr-petroglyphs.png'
-            />
-            <div  className="bottom">
-                <div style={left} className="left">
-                    
-                    <Instructions
-                    
-                    />
-                </div>
-                <div style={middle} className="middle">
-                    <Tile
-                        src='wildlife.js'
-                        title='Wildlife'
-                        image="horses.png"
-                    />
-                    <Tile
-                        src='family-history'
-                        title='Family History'
-                        image='corgi.png'
-                    />
-                    <Tile
-                        src='buildings'
-                        title='Buildings'
-                        image='pond.png'
-                    />
-                </div>
-                <div style={right} className="right">
-                    <Tile
-                        src='work-projects'
-                        title='Work Projects'
-                        image='river.png'
-                    /><Tile
-                        src='donations.js'
-                        title='Donations'
-                        image='clouds.png'
-                    /><Tile
-                        src='privacyPolicy.js'
-                        title='Privacy Policy'
-                        image='road.png'
-                    />
-                </div>
-            </div>
+  return (
+    <div>
+      <Head>
+        <link rel="stylesheet" href="/static/base.css" />
+      </Head>
+      <Nav />
+      <BannerImage
+        src="../static/bannerImageStorm.png"
+        alt="Stormy Image"
+        width="1704px"
+      />
+      <Break src="hr-train.png" />
+      <BannerImage
+        src="../static/carouselMist.png"
+        alt="Misty Image"
+        width="1278px"
+      />
+      <Break src="hr-tumbleweed.png" />
+      <div style={logInSection} className="logInSection">
+        <Third
+          header="fire watches"
+          name="Fire Warnings"
+          src="static/hr-fish.png"
+          paragarph="Fire Saftey is very important at the ranch, click here to check the current fire information."
+          url="https://www.nevadafireinfo.org/"
+          color="rgba(85, 135, 146, 0.5), rgba(107, 133, 62, 0.5)"
+          target="_blank"
+        />
+        <Third
+          header="login"
+          name="Login"
+          src="static/hr-blank.png"
+          paragarph="---"
+          url="/login"
+          color="rgba(109, 94, 65, 0.5), rgba(85, 135, 146, 0.5)"
+          target=""
+        />
+        <Third
+          header="weather"
+          name="Check The Weather"
+          src="static/hr-fish.png"
+          paragarph="Heading out soon? Click here to check the weather in the area."
+          url="https://www.nevadafireinfo.org/"
+          color="rgba(85, 135, 146, 0.5),rgba(107, 133, 62, 0.5)"
+          target="_blank"
+        />
+      </div>
+      <Break src="hr-petroglyphs.png" />
+      <div className="bottom">
+        <div style={left} className="left">
+          <Instructions />
         </div>
-    )
-}
+        <div style={middle} className="middle">
+          <Tile src="wildlife.js" title="Wildlife" image="horses.png" />
+          <Tile src="family-history" title="Family History" image="corgi.png" />
+          <Tile src="buildings" title="Buildings" image="pond.png" />
+        </div>
+        <div style={right} className="right">
+          <Tile src="work-projects" title="Work Projects" image="river.png" />
+          <Tile src="donations.js" title="Donations" image="clouds.png" />
+          <Tile
+            src="privacyPolicy.js"
+            title="Privacy Policy"
+            image="road.png"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 //this is a test
 export default Home;
--- server.js
+++ server.js
@@ -1,31 +1,61 @@
-const express = require('express')
-const next = require('next')
+// server.js
+/* eslint-disable no-console */
+const express = require("express");
+const next = require("next");
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const devProxy = {
+  "/.netlify": {
+    target: "http://localhost:9000",
+    pathRewrite: { "^/.netlify/functions": "" }
+  }
+};
 
+const port = parseInt(process.env.PORT, 10) || 3000;
+const env = process.env.NODE_ENV;
+const dev = env !== "production";
+const app = next({
+  dir: ".", // base directory where everything is, could move to src later
+  dev
+});
+
+const handle = app.getRequestHandler();
+
+let server;
 app
   .prepare()
   .then(() => {
-    const server = express()
+    server = express();
 
-    server.get('/p/:id', (req, res) => {
-      const actualPage = '/post'
-      const queryParams = { title: req.params.id }
-      app.render(req, res, actualPage, queryParams)
-    })
+    // Set up the proxy.
+    if (dev && devProxy) {
+      const proxyMiddleware = require("http-proxy-middleware");
+      Object.keys(devProxy).forEach(function(context) {
+        server.use(proxyMiddleware(context, devProxy[context]));
+      });
+    }
 
-    server.get('*', (req, res) => {
-      return handle(req, res)
-    })
+    // Default catch-all handler to allow Next.js to handle all other routes
+    server.all("*", (req, res) => handle(req, res));
+
+    // garrets stuff
+    server.get("/p/:id", (req, res) => {
+      const actualPage = "/post";
+      const queryParams = { title: req.params.id };
+      app.render(req, res, actualPage, queryParams);
+    });
+
+    server.get("*", (req, res) => {
+      return handle(req, res);
+    });
 
     server.listen(3000, err => {
-      if (err) throw err
-      console.log('> Ready on http://localhost:3000')
-    })
+      if (err) throw err;
+
+      console.log(`> Ready on port ${port} [${env}]`);
+      // console.log("> Ready on http://localhost:3000");
+    });
   })
   .catch(ex => {
-    console.error(ex.stack)
-    process.exit(1)
-  })
+    console.error(ex.stack);
+    process.exit(1);
+  });
