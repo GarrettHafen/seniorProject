58100b967c68035ba4a0606879cf4b9703e40b1c
Garrett Hafen <garrett.hafen12@gmail.com>
2019-07-05 23:55:10 -0600

    started calendar and adjusted styling
    finally got the calendar going! hope nothing happens after mergining

--- pages/calendar2.js
+++
@@ -1,8 +0,0 @@
-import FullCalendar from '../components/fullcalendar'
-export default function Home() {
-    return (
-        <div>
-            <FullCalendar defaultView='dayGridMonth' />
-        </div>
-    )
-}
