5c8d4ed647e95a1ac8facba9767f9c3a5414ff7c
Garrett Hafen <garrett.hafen12@gmail.com>
2019-07-06 14:45:46 -0600

    testing local storage authentication stuff

--- pages/listView.js
+++ pages/listView.js
@@ -13,59 +13,65 @@ const Wrapper = styled.div``;
 const Security = localStorage.getItem("authorized");
 
 if (!Security) {
-  alert("Please Sign In");
-  Router.push("/");
+    alert("Please Sign In");
+    Router.push("/");
 }
 
 const Home = ({ content: _ }) => {
-  return (
-    <Wrapper>
-      <Head>
-        <link rel="stylesheet" href="/static/base.css" />
-      </Head>
-      <Nav />
-      <BannerImage
-        src="../static/bannerImageStorm.png"
-        alt="Stormy Image"
-        width="1704px"
-      />
-      {_.listViewHeader && <H1 content={_.listViewHeader} />}
-      <ListItems
-        name={_.listViewItem[0].fields.listViewElementName}
-        desc={_.listViewItem[0].fields.listViewElementDescription}
-        img={_.listViewItem[0].fields.listViewElementImage.fields.file.url}
-        bgColor="#6B853E"
-        row="row"
-        textDirection="left"
-      />
-      <ListItems
-        name={_.listViewItem[1].fields.listViewElementName}
-        desc={_.listViewItem[1].fields.listViewElementDescription}
-        img={_.listViewItem[1].fields.listViewElementImage.fields.file.url}
-        bgColor="#6D5E41"
-        row="row-reverse"
-        textDirection="right"
-      />
-      <ListItems
-        name={_.listViewItem[2].fields.listViewElementName}
-        desc={_.listViewItem[2].fields.listViewElementDescription}
-        img={_.listViewItem[2].fields.listViewElementImage.fields.file.url}
-        bgColor="#558792"
-        row="row"
-        textDirection="left"
-      />
-    </Wrapper>
-  );
+    return (
+        <Wrapper>
+            <Head>
+                <link rel="stylesheet" href="/static/base.css" />
+            </Head>
+            <Nav />
+            <BannerImage
+                src="../static/bannerImageStorm.png"
+                alt="Stormy Image"
+                width="1704px"
+            />
+            {_.listViewHeader && <H1 content={_.listViewHeader} />}
+            <ListItems
+                name={_.listViewItem[0].fields.listViewElementName}
+                desc={_.listViewItem[0].fields.listViewElementDescription}
+                img={_.listViewItem[0].fields.listViewElementImage.fields.file.url}
+                bgColor="#6B853E"
+                row="row"
+                textDirection="left"
+            />
+            <ListItems
+                name={_.listViewItem[1].fields.listViewElementName}
+                desc={_.listViewItem[1].fields.listViewElementDescription}
+                img={_.listViewItem[1].fields.listViewElementImage.fields.file.url}
+                bgColor="#6D5E41"
+                row="row-reverse"
+                textDirection="right"
+            />
+            <ListItems
+                name={_.listViewItem[2].fields.listViewElementName}
+                desc={_.listViewItem[2].fields.listViewElementDescription}
+                img={_.listViewItem[2].fields.listViewElementImage.fields.file.url}
+                bgColor="#558792"
+                row="row"
+                textDirection="left"
+            />
+        </Wrapper>
+    );
 };
 
 Home.getInitialProps = async ({ asPath }) => {
-  const res = await contentful.query({
-    content_type: "listView",
-    "fields.pageUrl": asPath.split("/")[1]
-  });
+    const res = await contentful.query({
+        content_type: "listView",
+        "fields.pageUrl": asPath.split("/")[1]
+    });
+    const Security = localStorage.getItem("authorized");
 
-  return {
-    content: res.items[0].fields
-  };
+    if (!Security) {
+        alert("Please Sign In");
+        Router.push("/");
+    }
+    return {
+        content: res.items[0].fields
+    };
+
 };
 export default Home;
